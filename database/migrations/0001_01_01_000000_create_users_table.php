10e654584917e04c9c8bbf49b00b509f01eba80c
Julpa Denaldi <denaldi.j@gmail.com>
2024-05-06 12:46:29 +0700

    add organizationId to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique()->nullable();
-            $table->foreignIdFor(\App\Models\Organization::class)->nullable()->constrained()->cascadeOnDelete();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
